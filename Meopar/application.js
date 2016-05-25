6e0f6c44b41d475cb72ee82a54a8c99fb068d6da
Richard May <puzzleprogrammer@gmail.com>
2016-05-25 13:27:02 -0700

    Testing a redirect method 2.03

--- Meopar/application.js
+++ Meopar/application.js
@@ -29,7 +29,7 @@
 		
 //		console.log(x);
 		var todo = { text: String(label), _id: String(x) };
-        databaseTodosPut(todo).then(synchronize(),alert("it's not working4"))
+        databaseTodosPut(todo).then(synchronize())
         	.then(window.open("./thankyou.html"),alert("it's not working3"));
    //       .then(window.open('','_self').close());
 	}
