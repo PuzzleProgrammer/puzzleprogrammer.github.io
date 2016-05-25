aaae2f8773ec940d912806215c7aa091917b8023
Richard May <puzzleprogrammer@gmail.com>
2016-05-25 13:01:10 -0700

    Testing a redirect method 1.8

--- Meopar/application.js
+++ Meopar/application.js
@@ -26,6 +26,7 @@
 			("00"+d.getHours()).slice(-2)
 			+("00"+d.getMinutes()).slice(-2)
 			+("00"+d.getSeconds()).slice(-2));
+		alert("is it working?");
 //		console.log(x);
 		var todo = { text: String(label), _id: String(x) };
         databaseOpen().then(databaseTodosPut(todo)).then(synchronize())
