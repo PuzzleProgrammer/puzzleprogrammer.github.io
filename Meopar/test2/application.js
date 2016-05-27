2817da8aad2eea15cef24b2aff561f2e36b89580
Richard M <puzzleprogrammer@gmail.com>
2016-05-26 20:44:12 -0700

    updated appcache, added another test version

---
+++ Meopar/test2/application.js
@@ -0,0 +1,276 @@
+//(function() {
+//	var ul;
+
+	var api = 'https://hydrosurvey.herokuapp.com/todos';
+//	databaseOpen().then(function() {
+//      ul = document.querySelector('ul');
+//    });
+	document.ontouchmove = function(event){
+    	event.preventDefault();
+	}
+	function URLIdea(){
+		var name=window.location.href;
+		var num=name.substr(name.length - 1);
+		var label;
+		switch(Number(num)){
+			case 1:
+				label="19-24";
+				break;
+			case 2:
+				label="25-54";
+				break;
+			case 3:
+				label="55-75";
+				break;
+			case 4:
+				label="75+";
+				break;
+			case 5:
+				label="abstained";
+				break;
+/*			case 1:
+				label="9 and under";
+				break;
+			case 2:
+				label="10-14";
+				break;
+			case 3:
+				label="15-19";
+				break;
+			case 4:
+				label="20-69";
+				break;
+			case 5:
+				label="70+";
+				break;
+			case 6:
+				label="abstained";
+				break;
+*/			default:
+				label="error";
+		}
+		var d=new Date();
+		var x=
+			Number(
+			("00"+(d.getMonth()+1)).slice(-2)
+			+("00"+d.getDate()).slice(-2)
+			+d.getFullYear()
+			)+";"+Number(
+			("00"+d.getHours()).slice(-2)
+			+("00"+d.getMinutes()).slice(-2)
+			+("00"+d.getSeconds()).slice(-2));
+		var todo = { text: String(label), _id: String(x) };
+		databaseOpen().then(databaseTodosPut(todo)).then(synchronize);
+	}
+	function onClick(label,test){
+	if(typeof(test)==='undefined') test=false;
+		var timestamp=Date();
+//		alert("Age range: \t"+label+"\nTime:        \t"+timestamp);
+//		document.location.href="hydrocolor://";// this should be the URL of the hydrocolor app
+		//if there is no URL, the following will close the page
+		//window.open('','_self').close()
+		var d=new Date();
+//		console.log(d);
+		var x=
+			Number(
+			("00"+(d.getMonth()+1)).slice(-2)
+			+("00"+d.getDate()).slice(-2)
+			+d.getFullYear()
+			)+";"+Number(
+			("00"+d.getHours()).slice(-2)
+			+("00"+d.getMinutes()).slice(-2)
+			+("00"+d.getSeconds()).slice(-2));
+		
+//		console.log(x);
+		var todo = { text: String(label), _id: String(x) };
+//	if(!test){	
+	var prom=databaseOpen().then(databaseTodosPut(todo)).then(synchronize);
+//	when(prom).then(window.open("./thankyou.html"));
+//        	}
+//    if(test)  databaseOpen().then(databaseTodosPut(todo)).then(window.open("./thankyou.html"));
+   //       .then(window.open('','_self').close());
+	}
+	
+  function databaseTodosPut(todo) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['todo'], 'readwrite');
+      var store = transaction.objectStore('todo');
+      var request = store.put(todo);
+      transaction.oncomplete = resolve;
+      request.onerror = reject;
+    });
+  }
+
+  function databaseTodosGet(query) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['todo'], 'readonly');
+      var store = transaction.objectStore('todo');
+
+      // Get everything in the store
+      var keyRange = IDBKeyRange.lowerBound(0);
+      var cursorRequest = store.openCursor(keyRange);
+
+      // This fires once per row in the store, so for simplicity collect the data
+      // in an array (data) and send it pass it in the resolve call in one go
+      var data = [];
+      cursorRequest.onsuccess = function(e) {
+        var result = e.target.result;
+
+        // If there's data, add it to array
+        if (result) {
+//        console.log(result.value._id);
+		  if (!query || (query.deleted === true && result.value.deleted) || (query.deleted === false && !result.value.deleted)) {
+            data.push(result.value);
+          }
+          result.continue();
+
+        // Reach the end of the data
+        } else {
+          resolve(data);
+        }
+      };
+    });
+  }
+
+  function databaseOpen() {
+    return new Promise(function(resolve, reject) {
+      var version = 1;
+      var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+      var request = indexedDB.open('todos', version);
+
+      // Run migrations if necessary
+      request.onupgradeneeded = function(e) {
+        db = e.target.result;
+        e.target.transaction.onerror = reject;
+        db.createObjectStore('todo', { keyPath: '_id' });
+      };
+
+      request.onsuccess = function(e) {
+        db = e.target.result;
+        resolve();
+      };
+      request.onerror = reject;
+    });
+  }
+  
+  function refreshView() {
+    return databaseTodosGet({deleted:false}).then(renderAllTodos);
+  }
+  
+  function renderAllTodos(todos) {
+    var html = '<li>Date;Time;Age Range</li>';
+    todos.forEach(function(todo) {
+      html += todoToHtml(todo);
+    });
+//    alert("hi");
+    ul.innerHTML = html;
+  }
+
+  function flagAllTodos(todos) {
+    todos.forEach(function(todo) {
+      todo.deleted=true;
+      return databaseTodosPut(todo);
+    });
+  }
+
+  function todoToHtml(todo) {
+    return '<li>'+todo._id+';'+todo.text+'</li>';
+  }
+  
+  function dlete(){
+    if(confirm("Warning: This will permanently delete all entries on this page.\nOnly confirm if you have copied the values elsewhere already.")){
+databaseTodosGet({deleted:false}).then(flagAllTodos).then(synchronize()).then(refreshView);
+    }
+  }
+  
+  function databaseTodosDelete(todo) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['todo'], 'readwrite');
+      var store = transaction.objectStore('todo');
+      var request = store.delete(todo._id);
+      transaction.oncomplete = resolve;
+      request.onerror = reject;
+    });
+  }
+
+  function serverTodosGet(_id) {
+    return fetch(api + '/' + (_id ? _id : ''))
+      .then(function(response) {
+        return response.json();
+      });
+  }
+
+  function serverTodosPost(todo) {
+    return fetch(api, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+      })
+        .then(function(response) {
+          if (response.status === 410) throw new Error(response.statusText);
+      return response;
+    });
+  }
+
+  function serverTodosDelete(todo) {
+    return fetch(api + '/' + todo._id, { method: 'delete' })
+  }
+
+  function synchronize() {
+    return Promise.all([serverTodosGet(), databaseTodosGet()])
+      .then(function(results) {
+        var promises = [];
+        var remoteTodos = results[0];
+        var localTodos = results[1];
+
+        // Loop through local todos and if they haven't been
+        // posted to the server, post them.
+        promises = promises.concat(localTodos.map(function(todo) {
+          var deleteTodo = function() {
+            return databaseTodosDelete(todo);
+          };
+
+          // Has it been marked for deletion?
+          if (todo.deleted) {
+            return serverTodosDelete(todo).then(deleteTodo, function(res) {
+              if (err.message === "Gone") return deleteTodo();
+            });
+          }
+
+          // If this is a todo that doesn't exist on the server try to create
+          // it (if it fails because it's gone, delete it locally)
+          if (!arrayContainsTodo(remoteTodos, todo)) {
+            return serverTodosPost(todo)
+              .catch(function(err) {
+                if (err.message === "Gone") return deleteTodo(todo);
+              });
+          }
+        }));
+
+        // Go through the todos that came down from the server,
+        // we don't already have one, add it to the local db
+        promises = promises.concat(remoteTodos.map(function(todo) {
+          if (!arrayContainsTodo(localTodos, todo)) {
+            return databaseTodosPut(todo);
+          }
+        }));
+        return Promise.all(promises);
+    }, function(err) {
+      console.error(err, "Cannot connect to server");
+    });
+//    .then(refreshView);
+  }
+
+  function arrayContainsTodo(array, todo) {
+    for (var i = 0; i < array.length; i++) {
+       if(array[i]._id === todo._id) {
+         return true;
+       }
+    };
+    return false;
+  }
+
+//}());
